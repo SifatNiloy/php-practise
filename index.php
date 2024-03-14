fe1ca7133e8dae02def34194b2507b0ff344c8e9
SifatNiloy <SifatNiloy18@gmail.com>
2024-03-15 00:39:31 +0600

    variables and data types

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
+    //string
     $name = "John";
     $age = 25;
-    echo "hello {$name}! You are {$age} years old";
+    $price= 20.5;
+    $rate= 0.5;
+
+    $food= "pizza";
+    $quantity= 4;
+    $food_cost= 12.75;
+    $total_cost= $quantity * $food_cost;
+
+    $employed= true;
+    $for_sale= false;
+
+    echo "hello {$name}! <br> ";
+    echo "You are {$age} years old. <br>";
+    echo "my headphone price is \${$price} <br>";
+    echo "sales tax rate is {$rate}% <br>";
+
+    if($employed){
+        echo "You are employed <br>";
+    }else{
+        echo "You are unemployed <br>";
+    }
+
+    if($for_sale){
+        echo "The item is for sale <br>";
+    }else{  
+        echo "The item is not for sale <br>";
+    }   
+
+    echo "you have ordered {$quantity}X {$food} <br>";
+    echo "total pizza cost is \${$total_cost} <br>"
 ?>
